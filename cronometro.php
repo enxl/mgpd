a32594e996367ddbfc741c9d8804760e0b6bd504
enxl <uo287616@uniovi.es>
2025-11-27 18:36:38 +0100

    Añadidas décimas al cronómetro PHP

--- cronometro.php
+++ cronometro.php
@@ -16,17 +16,20 @@
         }
 
         public function arrancar() {
-            $this->inicio = time();
+            $this->inicio = microtime(true);
         }
 
         public function parar() {
-            $ahora = time();
+            $ahora = microtime(true);
             $this->tiempo = $ahora - $this->inicio;
         }
 
         public function mostrar() {
-            $tiempo_formateado = date('i:s', $this->tiempo);
-            return $tiempo_formateado;
+            $totalSegundos = $this->tiempo;
+            $minutos = floor($totalSegundos / 60);
+            $segundos = floor($totalSegundos % 60);
+            $decimas = floor(($totalSegundos - floor($totalSegundos)) * 10);
+            return sprintf("%02d:%02d.%01d", $minutos, $segundos, $decimas);
         }
     
     }
